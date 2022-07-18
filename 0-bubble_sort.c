12d6bd6029b5ea918501ed022231c2d53137103c
muthonimacharia <paullymuthoni@gmail.com>
2022-07-18 22:31:53 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,32 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ * bubble_sort - orders array with bubble sort algo
+ * @array: array to order
+ * @size: size of the array
+ * Return: array ordered
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int swap = 1, temp;
 
+	if (!array || size < 2)
+		return;
+	while (swap)
+	{
+		swap = 0;
+		for (i = 0; i < (size - 1) && i != (size - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+		}
+		i = 0;
+	}
+}
